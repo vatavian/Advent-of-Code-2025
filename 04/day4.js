a47f52597651efe12dbf10b68b5d4568d0850d2d
Mark Gray <mark-github@vatavia.net>
2025-12-06 17:18:08 -0500

    Day 4 part 2: remove successive batches of accessible rolls

--- 04/day4.js
+++ 04/day4.js
@@ -1,6 +1,7 @@
 // Day 4: Printing Department
 
 let rollRows = [];
+let accessibleRollsMap = [];
 let numRows = 0;
 let numCols = 0;
 
@@ -18,7 +19,7 @@ function countNeighbors(checkRow, checkCol) {
     return count;
 }
 
-function accessibleRolls() {
+function accessibleRollsCount() {
     let accessible = 0;
     for (let scanRow = 0; scanRow < numRows; scanRow++)
         for (let scanCol = 0; scanCol < numCols; scanCol++) {
@@ -29,17 +30,33 @@ function accessibleRolls() {
 }
 
 async function readFile(filePath) { 
-     try {
+    try {
         const file = Bun.file(filePath);
         const content = await file.text();
-        rollRows = content.split(/\r?\n/).map(rowText => rowText.split(''));
+        const origRollRows = content.split(/\r?\n/).map(rowText => rowText.split(''));
+        rollRows = origRollRows.map(row => row.slice());
         numRows = rollRows.length;
         numCols = rollRows[0].length;
-
-        console.log('accessibleRolls part 1:', accessibleRolls());
-      } catch (error) {
+        console.log('accessibleRollsCount (part 1):', accessibleRollsCount());
+        let totalRemoved = 0;
+        let removedThisRound = 0;
+        do {
+            removedThisRound = 0;
+            const accessibleRollsThisRound = rollRows.map(row => row.slice());
+            for (let scanRow = 0; scanRow < numRows; scanRow++)
+                for (let scanCol = 0; scanCol < numCols; scanCol++)
+                    if (rollRows[scanRow][scanCol] === '@' && countNeighbors(scanRow, scanCol) < 4) {
+                        accessibleRollsThisRound[scanRow][scanCol] = 'x';
+                        removedThisRound++;
+                    }
+            // console.log("Remove", removedThisRound);
+            totalRemoved += removedThisRound;
+            rollRows = accessibleRollsThisRound;
+        } while (removedThisRound > 0);
+        console.log("Part 2: Removed a total of ", totalRemoved);
+    } catch (error) {
         console.error('Error reading file:', error);
-      }
+    }
 }
 
 readFile('./input.txt');

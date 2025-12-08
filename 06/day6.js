35b9187af83fa121996257a9d295eb087970b981
Mark Gray <mark-github@vatavia.net>
2025-12-08 18:14:03 -0500

    Day 6 Part 2: Trash Compactor
    This time read the numbers completely differently.

--- 06/day6.js
+++ 06/day6.js
@@ -1,7 +1,10 @@
 // Day 6: Trash Compactor
 
 const AVAILABLE_OPERATORS = '+*';
+const OPERATORS_RE = /[\+\*]/;
 let values = [];
+let lines = [];
+let operatorsLine = '';
 
 function addVals1(index) {
     let total = 0n;
@@ -17,26 +20,60 @@ function multVals1(index) {
     return total;
 }
 
+function getColumnValues2(startIndex, endBeforeIndex) {
+    const vals = {};
+    for (let index=startIndex; index< endBeforeIndex; index++) {
+        lines.forEach( line => {
+            const ch = line[index].trim();
+            if (ch) {
+                vals[index] = `${vals[index] || ''}${ch}`;
+            }
+        })
+    }
+    return Object.values(vals);
+}
+
 async function readFile(filePath) { 
     try {
         const file = Bun.file(filePath);
         const content = await file.text();
         let operators = [];
-        content.split(/\r?\n/).forEach(line => {
+        lines = content.split(/\r?\n/);
+        lines.forEach(line => {
             const vals = line.trim().split(/\s+/);
             if (vals[0].length === 1 && AVAILABLE_OPERATORS.indexOf(vals[0]) > -1)
                 operators = vals;
             else
                 values.push(vals.map(v => BigInt(v)));
         });
-        // console.log(values);
-        // console.log(JSON.stringify(operators));
         let grandTotal1 = 0n;
         operators.forEach((op, index) => {
-            // console.log(op);
             grandTotal1 += (op === '+') ? addVals1(index) : multVals1(index);
         })
         console.log('Part 1: grand total', grandTotal1);
+
+        let grandTotal2 = 0n;
+        operatorsLine = lines.pop();
+        let lastOpIndex = operatorsLine.length;
+        for (let opIndex=Math.max(operatorsLine.lastIndexOf('+'), operatorsLine.lastIndexOf('*'));
+             opIndex >= 0;
+             opIndex=Math.max(operatorsLine.lastIndexOf('+', opIndex-1), operatorsLine.lastIndexOf('*', opIndex-1)))
+        {
+            const op = operatorsLine.substring(opIndex, opIndex + 1);
+            const colValues = getColumnValues2(opIndex, lastOpIndex);
+            // console.log('op', op, opIndex, '-', lastOpIndex, 'colValues', colValues);
+            if (op == '+') {
+                colValues.forEach(v => grandTotal2 += BigInt(v));
+            } else {
+                let mult = 1n;
+                colValues.forEach(v => mult *= BigInt(v));
+                grandTotal2 += mult;
+            }
+            lastOpIndex = opIndex;
+            if (lastOpIndex === 0) break;
+        }
+        console.log('Part 2: grand total', grandTotal2);
+
     } catch (error) {
         console.error('Error reading file:', error);
     }
